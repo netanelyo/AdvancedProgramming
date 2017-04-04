2223ca0f3e6fa15327945c0bde411a3f134620ab
etanel yosephian <nati.yosephian@gmail.com>
2017-04-04 21:03:03 +0300

    FBattle

--- AdvancedProgramming-HW1/FBattleshipGameAlgo.h
+++ AdvancedProgramming-HW1/FBattleshipGameAlgo.h
@@ -3,22 +3,22 @@
 #include "IBattleshipGameAlgo.h"
 #include <fstream>
 
+#define BOARD_SIZE 10
+
 class FBattleshipGameAlgo : public IBattleshipGameAlgo
 {
 public:
-	FBattleshipGameAlgo(const std::string movesFile) : board(nullptr), numRows(0), numCols(0), points(0),
+	FBattleshipGameAlgo(const std::string movesFile) : points(0),
 							shipCounter(0), playerMoves(movesFile) {}
-	~FBattleshipGameAlgo();
+	~FBattleshipGameAlgo() {}
 	void setBoard(const char** board, int numRows, int numCols) override;
 	std::pair<int, int> attack() override;													
 	void notifyOnAttackResult(int player, int row, int col, AttackResult result) override;
 private:
-	char** board;
-	uint16_t numRows;
-	uint16_t numCols;
-	uint16_t points;
-	uint16_t shipCounter;
-	std::ifstream playerMoves;
+	char			board[BOARD_SIZE][BOARD_SIZE];
+	uint16_t		points;
+	uint16_t		shipCounter;
+	std::ifstream	playerMoves;
 };
 
 
