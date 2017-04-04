5264696220db9bc15f08368a4ad0b92661c4d0d7
etanel yosephian <nati.yosephian@gmail.com>
2017-04-04 19:14:11 +0300

    asdcas

--- AdvancedProgramming-HW1/Game.cpp
+++ AdvancedProgramming-HW1/Game.cpp
@@ -1,16 +1,15 @@
 #include "Game.h"
 
 
-Game::Game(int rows, int columns, std::string movesFileA, std::string movesFileB) : 
+Game::Game(int rows, int columns, std::string movesFileA, std::string movesFileB) :
 	nextPlayer(0), A(movesFileA), B(movesFileB), rowsNum(rows), colsNum(columns)
 {
 	gameBoard = new char*[rows];
 
 	for (int i = 0; i < rows; i++)
 	{
-		gameBoard[i] = new char[columns]; 
+		gameBoard[i] = new char[columns];
 	}
-
 }
 
 Game::~Game()
