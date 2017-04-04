dcb8d6a472f7c1228002a95291a599748b80d235
etanel yosephian <nati.yosephian@gmail.com>
2017-04-04 22:20:53 +0300

    FBattle - notifyOnAttack

--- AdvancedProgramming-HW1/FBattleshipGameAlgo.cpp
+++ AdvancedProgramming-HW1/FBattleshipGameAlgo.cpp
@@ -2,10 +2,9 @@
 
 void FBattleshipGameAlgo::setBoard(const char** board, int numRows, int numCols)
 {
-	for (int i = 0; i < numRows; ++i) {
+	for (int i = 0; i < numRows; ++i)
 		for (int j = 0; j < numCols; ++j)
 			this->board[i][j] = board[i][j];
-	}
 }
 
 std::pair<int, int> FBattleshipGameAlgo::attack()
@@ -16,5 +15,15 @@ std::pair<int, int> FBattleshipGameAlgo::attack()
 
 void FBattleshipGameAlgo::notifyOnAttackResult(int player, int row, int col, AttackResult result)
 {
-
+	if (board[row][col] != '0')
+		switch (result)
+		{
+		case AttackResult::Sink:
+			shipCounter--;
+		case AttackResult::Hit:
+			board[row][col] = 'X';
+			break;
+		default:
+			break;
+		}
 }
