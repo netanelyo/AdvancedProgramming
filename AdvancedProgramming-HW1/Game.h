cd6f3663e19cad247855764dd8e045dc8c8afbee
Shahar <shaharrat@gmail.com>
2017-04-04 18:53:55 +0300

    Game commit

--- AdvancedProgramming-HW1/Game.h
+++ AdvancedProgramming-HW1/Game.h
@@ -3,10 +3,29 @@
 
 using Player = FBattleshipGameAlgo;
 
+enum class GameState 
+{
+	GAME_OVER, 
+	CONTINUE_PLAYING
+};
+
 class Game
 {
 public:
+	Game(int rows, int columns, std::string movesFileA, std::string movesFileB);
+	~Game(); 
+	int checkAndCreateBoard(std::ifstream & BoardFile); 
+	GameState playMove(); 
+	void createBoardsForPlayers(); 
 
 private:
+	int nextPlayer; 
+	Player A;
+	Player B; 
+	int rowsNum;
+	int colsNum;
+	char** gameBoard; 
+	 
 	
+
 };
