9834b777b3fbd7cfbb87d02c8051759466856f0c
etanel yosephian <nati.yosephian@gmail.com>
2017-04-08 13:37:06 +0300

    isDone member added

--- AdvancedProgramming-HW1/FBattleshipGameAlgo.cpp
+++ AdvancedProgramming-HW1/FBattleshipGameAlgo.cpp
@@ -1,4 +1,5 @@
 #include "FBattleshipGameAlgo.h"
+#include <string>
 
 void FBattleshipGameAlgo::setBoard(const char** board, int numRows, int numCols)
 {
@@ -9,13 +10,15 @@ void FBattleshipGameAlgo::setBoard(const char** board, int numRows, int numCols)
 
 std::pair<int, int> FBattleshipGameAlgo::attack()
 {
-
+	std::string line("");
+	//std::getline(playerMoves, )
 	return std::pair<int, int>();
 }
 
 void FBattleshipGameAlgo::notifyOnAttackResult(int player, int row, int col, AttackResult result)
 {
 	if (board[row][col] != '0')
+	{
 		switch (result)
 		{
 		case AttackResult::Sink:
@@ -26,4 +29,5 @@ void FBattleshipGameAlgo::notifyOnAttackResult(int player, int row, int col, Att
 		default:
 			break;
 		}
+	}
 }
--- AdvancedProgramming-HW1/FBattleshipGameAlgo.h
+++ AdvancedProgramming-HW1/FBattleshipGameAlgo.h
@@ -9,16 +9,18 @@ class FBattleshipGameAlgo : public IBattleshipGameAlgo
 {
 public:
 	FBattleshipGameAlgo(const std::string movesFile) : points(0),
-							shipCounter(0), playerMoves(movesFile) {}
+							shipCounter(0), playerMoves(movesFile), isDone(false) {}
 	~FBattleshipGameAlgo() {}
 	void setBoard(const char** board, int numRows, int numCols) override;
 	std::pair<int, int> attack() override;													
 	void notifyOnAttackResult(int player, int row, int col, AttackResult result) override;
+	bool getIsDone() const {return isDone;}
 private:
 	char			board[BOARD_SIZE][BOARD_SIZE];
 	uint16_t		points;
 	uint16_t		shipCounter;
 	std::ifstream	playerMoves;
+	bool			isDone;
 };
 
 
