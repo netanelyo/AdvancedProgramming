f1b81f027e606c0f7e204e89b4ec6a321409224a
Shahar <shaharrat@gmail.com>
2017-06-09 14:18:20 +0300

    DataImpl little change

--- AdvancedProgramming-HW3Official/BoardDataImpl.cpp
+++ AdvancedProgramming-HW3Official/BoardDataImpl.cpp
@@ -4,10 +4,11 @@
 
 char BoardDataImpl::charAt(Coordinate c) const
 {
-	auto sq = m_gameBoard.getBoardSquare(c);
+	auto sq = m_board.getBoardSquare(c);
 	if (sq != BattleshipGameUtils::Constants::EMPTY_SIGN)
 	{
-		if ((isupper(sq) && m_player == 0) || (islower(sq) && m_player == 1))
+		if ((isupper(sq) && m_player == BattleshipGameUtils::Constants::PLAYER_A) || 
+			(islower(sq) && m_player == BattleshipGameUtils::Constants::PLAYER_B))
 			return sq;
 		return BattleshipGameUtils::Constants::EMPTY_SIGN;
 	}
