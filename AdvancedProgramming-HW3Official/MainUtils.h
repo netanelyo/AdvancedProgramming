2621214e3480d9e54b3b902af362285ab8af46eb
Shahar <shaharrat@gmail.com>
2017-06-09 22:34:51 +0300

    MainUtils

---
+++ AdvancedProgramming-HW3Official/MainUtils.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+
+class MainUtils
+{
+public:
+	static const int DEFAULT_THREAD_NUM = 4;
+	static const char COMMENT_SIGN = '#';
+	static const char EQUALS = '=';
+
+	static const std::string DEFAULT_LOGFILE_NAME;
+	static const std::string CONFIG_END;
+	static const std::string WORKING_DIR;
+	static const std::string SEPERATOR;
+	static const std::string ASTERISK;
+	static const std::string THREADS;
+	static const std::string LOGGER_NAME;
+	static const std::string LOGGER_LEVEL;
+
+	static int parseArgs(int argc, char** argv, std::string& dirPath);
+	static char ** checkIfExists(char ** begin, char ** end, const std::string & option);
+	static std::string getDefaultParamsFromConfig(int& numOfThreads, std::ifstream& config); 
+};
